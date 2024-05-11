925f217881152b5cb25379c8fc453daa5fe6117a
Kartik Pal <kartik.p1110@gmail.com>
2024-05-11 19:45:19 +0530

    feat: added navigation menu

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
+const { fontFamily } = require("tailwindcss/defaultTheme")
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  darkMode: ["class"],
+  content: ["app/**/*.{ts,tsx}", "components/**/*.{ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["var(--font-sans)", ...fontFamily.sans],
+      },
+    },
   },
-  plugins: [],
 }
-
